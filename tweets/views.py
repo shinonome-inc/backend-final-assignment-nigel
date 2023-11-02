e76ceb41df5bc15ae5a742d20dca54dd5817d411
Nigel <nigelwijaya7@gmail.com>
2023-11-02 23:35:56 +0900

    Create all tweet list view

--- tweets/views.py
+++ tweets/views.py
@@ -1,14 +1,17 @@
-# from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView  # ListView
-from django.views.generic.base import TemplateView
+from django.views.generic import CreateView, ListView
 
 from .forms import CreateTweetForm
+from .models import Tweet
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(LoginRequiredMixin, ListView):
+    model = Tweet
     template_name = "tweets/home.html"
+
+    def get_queryset(self):
+        return Tweet.objects.order_by("-timestamp")
 
 
 class TweetCreateView(LoginRequiredMixin, CreateView):
